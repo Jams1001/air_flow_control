6495fea7a011e8265f688789612d187154e56228
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2023-02-25 10:49:43 -0600

    Archivos de Chien: su simulación y experimental. Leer comentarios.

--- resultadoChienlab.m
+++ resultadoChienlab.m
@@ -9,13 +9,50 @@ r = data(:,2);
 y = data(:,3);
 u = data(:,4);
 
-
+tn = t - mean(t(1:10));
 yn = y - mean(y(1:10));
 rn = r - mean(r(1:10));
 un = u - mean(u(1:10));
+
 figure(1);
-plot(t, rn, 'r', t, yn, 'g',t, un, 'b');
+plot(tn, rn, 'r', tn, yn, 'g',tn, un, 'b');
 xlabel ('Tiempo (s)') ;
 ylabel ('Respuesta del sistema ') ;
 legend ('r(s)','y(s)','u(s)') %leyenda
 grid on;
+
+
+
+% Parámetros
+
+% Para realizar el cálculo de parámetros es necesario utilizar los vectores
+% normalizados.
+
+% Error permanente promedio
+Eprem = mean(abs(rn-yn))
+
+% Se obtiene el sobrepaso máximo no normalizado para luego comparar con el
+% valor máximo y normalizar el sobrepaso.
+[Ymax1, indymax1] = max(yn,[],'all')
+
+% Valor final de la respuesta: utilizo el promedio de los últimos 851 datos
+Yf1 = mean(yn(2556:3407))
+
+% Máximo de la señal realimentada 
+Ymax = max(abs(max(yn)),abs(min(yn)))
+
+% Sobrepaso máximo normalizado
+Mpnr = 100*(Ymax1-Ymax)/(Ymax)
+
+% Posición del vector de respuesta cuando llega a la banda del 2%
+% Busca el índice del valor que es mayor al 2% del valor mááximo de la
+% señal realimentada y se le resta uno.
+ind2per = find(yn>Ymax-0.02*(Ymax),1)-1
+
+% Tiempo de asentamiento al 2%
+% Busca en el vector del tiempo el índice del dato asociado al 2%
+t2per1 = tn(ind2per)
+
+% IAE
+IAE = trapz(tn,abs(rn-yn))
+
--- sintonizacionChien.m
+++ sintonizacionChien.m
@@ -2,6 +2,10 @@ clc;
 clear;
 close all;
 
+% Parte I: Carga de datos
+% Se utiliza para observar el comportamiento real de la planta
+% No se utiliza para nada más
+
 % Carga de datos
 data = readmatrix('Flujo_delta_25a50.xlsx','Range','A377:C1471')
 %data = readmatrix('Flujo_delta_25a50_sin_ruido.xlsx')
@@ -9,7 +13,7 @@ data = readmatrix('Flujo_delta_25a50.xlsx','Range','A377:C1471')
 %data = readmatrix('Flujo_delta_25a50.xlsx')
 
 
-t = data(:,1)-30;
+tp = data(:,1)-30;
 u = data(:,2);
 y = data(:,3);
 
@@ -20,6 +24,8 @@ un = u - mean(u(1:10))
 %figure(1);
 %plot(t, un, 'r', t, yn, 'g');
 
+
+% Parte II: Simulación de respuesta de sistema con controlador Chien
 
 s = tf("s");
 % Datos de la plata
@@ -34,27 +40,26 @@ tao = L/T % tao se encuentra dentro del rango
 % Planta
 P=(Kp*exp(-L*s))/(1+T*s)
 
-% Controlador Reswick
-%{
-Kc = 0.15/Kp
-Ti2 = 0.16*tao
-C3 = Kc* (( Ti2*s + 1)/(Ti2*s))
-%}
-
 % Controlador Chien
 
 Kc = (0.35*T)/(Kp*L)
 Ti2 = 1.17*T
 C3 = Kc* (( Ti2*s + 1)/(Ti2*s))
-L = P*C3
+L1 = P*C3
 
-sisotool(C3)
-figure(1);
-bode(L)
-margin(L)
-grid on
+%sisotool(C3)
+%figure(1);
+%bode(L)
+%margin(L)
+%grid on
 
-% Pruebas con controlador Shinskey
+
+
+
+% Pruebas con controlador Chien
+
+% Vector de tiempo para respuesta de sistema simulado. NO CONFUNDIR CON
+% VECTOR DE TIEMPO utilizado para calcular parámetros
 
 t = [0:0.01:100];
 % Servocontrol
@@ -62,6 +67,10 @@ Myr = ( C3 * P ) /(1+ C3 * P );
 % Regulador
 Myd = minreal( P /(1+ C3 * P ) ); 
 
+% F.T de la planta: Se hizo para tratar de simular la respuesta de la
+% planta, sin embargo, no se ha podido simular la respuesta de la planta.
+Myrp = ( P ) /(1+ P );
+ 
 r = 0; %referencia
 r ( t >= 1) = 1;
 
@@ -75,13 +84,68 @@ yr = lsim ( Myr , r , t );
 yd = lsim ( Myd , d , t );
 y = yr + yd;
 
-figure (2) ;
-title ('Respuesta del sistema como Servocontrol y como Regulador ') ;
-plot (t,d,'--',t,y,t,r)
-xlabel ('Tiempo (s)') ;
-ylabel ('Respuesta del sistema ') ;ylabel ('Respuesta del sistema ') ;
-legend ('d(s)','y(s)','r(s)') %leyenda
-grid on;
 
+%figure (2) ;
+%title ('Respuesta del sistema como Servocontrol y como Regulador ') ;
+%plot (t,d,'--',t,y,t,r)
+%xlabel ('Tiempo (s)') ;
+%ylabel ('Respuesta del sistema ') ;ylabel ('Respuesta del sistema ') ;
+%legend ('d(s)','y(s)','r(s)') %leyenda
+%grid on;
+
+% Parte III: obtención de parámetros
+
+% Se realiza la simulación de esta forma para poder calcular parámetros
+% Si se utilizan los tiempos de simulación de la parte anterior, se
+% presenta un error debido a que la cantidad de elementos en vectores no
+% coincide.
+
+Myr= L1/(1+L1);
+r2=heaviside(t-1);ss
+r1 = r2*25;
+y1=lsim(Myr,r1,t);
+
+%{
+% Salida planta
+yp = lsim (Myrp , r , t )
+%}
+
+%yplanta = lsim(Myrp,r,t);
+
+plot(t,r1,t, y1,"LineWidth",1.5)
+hold on
+grid on
+legend("entrada","y(t) ")
+title("Respuesta realimentada controlador Chien")
+xlabel("Tiempo (s)")
+ylabel("Amplitud")
+figure(1)
+hold off
+
+%{
+plot(t,r,t, yplanta,"LineWidth",1.5)
+hold on
+grid on
+legend("entrada","y(t) ")
+title("Salida de planta")
+xlabel("Tiempo (s)")
+ylabel("Amplitud")
+figure(2)
+hold off
+%}
+
+
+% IAE
+
+% Se debe tratar más debido a que el error se está calculando utilizando el
+% valor deseado, no la  salida de la planta.
+
+% Para poder calcularlo, ES NECESARIO SIMULAR LA RESPUESTA DE LA PLANTA
+% sin compensar para poder calcular el error y así poder integrarlo.
+
+SalidaPlanta= r1;
+SalidaModelo= y1; % Obtenido con toolbox
+e_IAE=abs(SalidaPlanta-SalidaModelo);
+IAE=trapz(t,e_IAE)
 
 
